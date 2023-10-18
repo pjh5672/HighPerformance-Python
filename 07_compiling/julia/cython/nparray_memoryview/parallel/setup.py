90370bfc305af8a51aa58863719a776cdf90fdc4
pjh5672 <pjh5672.dev@gmail.com>
2023-10-18 18:35:53 +0900

    add aot, jit compile test

---
+++ 07_compiling/julia/cython/nparray_memoryview/parallel/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from distutils.extension import Extension
+import numpy as np
+
+ext_modules = [Extension(
+      "cythonfn3",
+      ["cythonfn3.pyx"], 
+      
+      extra_compile_args=['-fopenmp'], 
+      extra_link_args=['-fopenmp'],
+      )]
+
+from Cython.Build import cythonize
+setup(ext_modules=cythonize(ext_modules, compiler_directives={"language_level": "3"},),include_dirs=[np.get_include()]) 
---
+++ 07_compiling/julia/cython/nparray_memoryview/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+import numpy as np
+
+from Cython.Build import cythonize
+setup(ext_modules=cythonize("cythonfn2.pyx", compiler_directives={"language_level": "3"}),
+        include_dirs=[np.get_include()])
